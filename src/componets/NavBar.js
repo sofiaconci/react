73a5b5d2b6e2a78441abe7edf29b22f7dedc6c38
Sofi <sofiaconci@gmail.com>
2022-04-23 17:55:16 -0300

    first commit

---
+++ src/componets/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+    return (
+
+        <div> 
+            <li>Inicio</li>
+            <li>Abrigos</li>
+            <li>Pantalones</li>
+            <li>Remeras</li>
+        </div>
+        
+
+
+    )
+}
+
+export default NavBar
